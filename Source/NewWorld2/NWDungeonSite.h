5fd5e870dd9be764f4b39512675fb887c38b295b
Bicalho <74017831+LuizBicalho3508@users.noreply.github.com>
2026-09-11 16:54:38 -0300

    fix: replicate dungeon type seed and tier to clients

--- Source/NewWorld2/NWDungeonSite.h
+++ Source/NewWorld2/NWDungeonSite.h
@@ -20,6 +20,8 @@ class NEWORLD2_API ANWDungeonSite : public AActor
 public:
     ANWDungeonSite();
 
+    virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
     void ConfigureDungeon(ENWDungeonType InType, int32 InSeed, int32 InTier);
 
     UFUNCTION(BlueprintPure, Category="Dungeon")
@@ -39,6 +41,9 @@ private:
     UStaticMesh* FindInstalledMesh(const TArray<FString>& Keywords) const;
     FName GetDungeonThemeName() const;
 
+    UFUNCTION()
+    void OnRep_DungeonConfiguration();
+
     UPROPERTY(VisibleAnywhere)
     TObjectPtr<USceneComponent> SceneRoot;
 
@@ -51,13 +56,13 @@ private:
     UPROPERTY(VisibleAnywhere)
     TObjectPtr<UTextRenderComponent> DungeonLabel;
 
-    UPROPERTY(EditAnywhere, Category="Dungeon")
+    UPROPERTY(ReplicatedUsing=OnRep_DungeonConfiguration, EditAnywhere, Category="Dungeon")
     ENWDungeonType DungeonType = ENWDungeonType::DarkCastle;
 
-    UPROPERTY(EditAnywhere, Category="Dungeon")
+    UPROPERTY(ReplicatedUsing=OnRep_DungeonConfiguration, EditAnywhere, Category="Dungeon")
     int32 DungeonSeed = 41001;
 
-    UPROPERTY(EditAnywhere, Category="Dungeon", meta=(ClampMin="1", ClampMax="10"))
+    UPROPERTY(ReplicatedUsing=OnRep_DungeonConfiguration, EditAnywhere, Category="Dungeon", meta=(ClampMin="1", ClampMax="10"))
     int32 DungeonTier = 2;
 
     UPROPERTY(EditAnywhere, Category="Dungeon")
