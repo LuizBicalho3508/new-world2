e4438c21c10635897d2c5a1c869ac881bce9dfc9
Bicalho <74017831+LuizBicalho3508@users.noreply.github.com>
2026-09-11 16:45:07 -0300

    feat: expose armor weight and legendary dungeon loot helpers

--- Source/NewWorld2/NWCombatLibrary.h
+++ Source/NewWorld2/NWCombatLibrary.h
@@ -11,8 +11,10 @@ namespace NWCombat
     NEWORLD2_API ENWWeaponType GetNextWeaponType(ENWWeaponType WeaponType);
     NEWORLD2_API FString RarityToString(ENWItemRarity Rarity);
     NEWORLD2_API FString EquipmentSlotToString(ENWEquipmentSlot Slot);
+    NEWORLD2_API FString ArmorWeightToString(ENWArmorWeight ArmorWeight);
     NEWORLD2_API FString AffixToString(ENWAffixType Affix);
     NEWORLD2_API FLinearColor RarityColor(ENWItemRarity Rarity);
     NEWORLD2_API FNWGeneratedItem GenerateProceduralItem(int32 LootSeed, int32 WorldEpoch = 1);
+    NEWORLD2_API FNWGeneratedItem GenerateLegendaryDungeonItem(int32 LootSeed, int32 WorldEpoch, FName DungeonTheme);
     NEWORLD2_API float GetItemScore(const FNWGeneratedItem& Item);
 }
