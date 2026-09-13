89a97d86cb77eeb92f15e4199b6ebb4b8fea9fc7
Bicalho <74017831+LuizBicalho3508@users.noreply.github.com>
2026-09-13 15:16:00 -0400

    feat: redesign premium hud and inventory data model

--- Source/NewWorld2/NWCombatHUDWidget.h
+++ Source/NewWorld2/NWCombatHUDWidget.h
@@ -7,7 +7,9 @@
 class ANWCharacter;
 class UBorder;
 class UProgressBar;
+class UScrollBox;
 class UTextBlock;
+class UVerticalBox;
 
 UCLASS()
 class NEWORLD2_API UNWCombatHUDWidget : public UUserWidget
@@ -26,50 +28,36 @@ protected:
 private:
     void BuildHUD();
     void RefreshHUD();
-    void RefreshInventory();
+    void RefreshInventory(bool bForce = false);
+    uint32 BuildInventorySignature(const ANWCharacter* Character) const;
     UTextBlock* MakeText(const FString& InitialText, int32 FontSize);
+    UBorder* MakePanel(const FLinearColor& Color, const FMargin& Padding);
+    FString FormatAffixes(const struct FNWGeneratedItem& Item) const;
 
     TWeakObjectPtr<ANWCharacter> ObservedCharacter;
 
-    UPROPERTY(Transient)
-    TObjectPtr<UProgressBar> HealthBar;
+    UPROPERTY(Transient) TObjectPtr<UProgressBar> HealthBar;
+    UPROPERTY(Transient) TObjectPtr<UProgressBar> StaminaBar;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> HealthText;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> StaminaText;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> WeaponText;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> StatsText;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> StateText;
+    UPROPERTY(Transient) TArray<TObjectPtr<UTextBlock>> AbilityTexts;
+    UPROPERTY(Transient) TArray<TObjectPtr<UTextBlock>> AbilityDescriptionTexts;
+    UPROPERTY(Transient) TArray<TObjectPtr<UBorder>> AbilityCards;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> LootPromptText;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> CrosshairText;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> HelpText;
 
-    UPROPERTY(Transient)
-    TObjectPtr<UProgressBar> StaminaBar;
-
-    UPROPERTY(Transient)
-    TObjectPtr<UTextBlock> HealthText;
-
-    UPROPERTY(Transient)
-    TObjectPtr<UTextBlock> StaminaText;
-
-    UPROPERTY(Transient)
-    TObjectPtr<UTextBlock> WeaponText;
-
-    UPROPERTY(Transient)
-    TObjectPtr<UTextBlock> StatsText;
-
-    UPROPERTY(Transient)
-    TObjectPtr<UTextBlock> StateText;
-
-    UPROPERTY(Transient)
-    TArray<TObjectPtr<UTextBlock>> AbilityTexts;
-
-    UPROPERTY(Transient)
-    TObjectPtr<UTextBlock> LootPromptText;
-
-    UPROPERTY(Transient)
-    TObjectPtr<UTextBlock> CrosshairText;
-
-    UPROPERTY(Transient)
-    TObjectPtr<UTextBlock> HelpText;
-
-    UPROPERTY(Transient)
-    TObjectPtr<UBorder> InventoryPanel;
-
-    UPROPERTY(Transient)
-    TObjectPtr<UTextBlock> InventoryText;
+    UPROPERTY(Transient) TObjectPtr<UBorder> InventoryPanel;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> InventoryTitleText;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> EquippedText;
+    UPROPERTY(Transient) TObjectPtr<UTextBlock> InventoryHintText;
+    UPROPERTY(Transient) TObjectPtr<UVerticalBox> InventoryItemsBox;
+    UPROPERTY(Transient) TObjectPtr<UScrollBox> InventoryScroll;
 
     bool bInventoryVisible = false;
     float RefreshAccumulator = 0.0f;
+    uint32 LastInventorySignature = 0;
 };
